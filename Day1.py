27f69ee3df3720aa7ccac9a7fae6d2b685cdf4b7
Om Panchal <Panchalom136@gmail.com>
2025-06-26 16:48:16 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,16 @@
+# Print message
+print("Hello, Python!")
+
+# Variables
+name = "Om"
+age = 20
+print("My name is", name, "and I am", age, "years old.")
+
+# User input
+your_name = input("What is your name? ")
+print("Nice to meet you,", your_name)
+
+# Simple math
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print("Their sum is:", num1 + num2)
